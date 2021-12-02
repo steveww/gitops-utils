063eef78fe39f94e046bd6b8e1df18f73ba2ab7b
SteveWW <s@steveww.org>
2021-12-02 17:07:07 +0000

    improve format

--- ssh-fix.sh
+++ ssh-fix.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-kubectl exec -ti deployment/mccp-chart-cluster-service -n wego-system -- sh -c "ssh-keyscan github.com gitlab.com bitbucket.org ssh.dev.azure.com vs-ssh.visualstudio.com > /root/.ssh/known_hosts"
+kubectl exec \
+    -it \
+    -n wego-system \
+    deployment/mccp-chart-cluster-service \
+    -- sh -c "ssh-keyscan github.com > /root/.ssh/known_hosts"
 
